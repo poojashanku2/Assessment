2c0f512d816052543b506135878e3a87a0f2edf6
poojashanku2 <145142011+poojashanku2@users.noreply.github.com>
2023-09-19 19:47:44 +0530

    Add files via upload

---
+++ arith.py
@@ -0,0 +1,12 @@
+#Python program to understand the arithmetical operator addition  
+   
+num1 = input('Enter first number: ')    
+num2 = input('Enter second number: ')       
+sum = float(num1) + float(num2)      
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+min = float(num1) - float(num2) 
+print('The subtraction of {0} and {1} is {2}'.format(num1, num2, min))
+mul = float(num1) * float(num2)    
+print('The multiplication of {0} and {1} is {2}'.format(num1, num2, mul))
+div = float(num1) - float(num2)       
+print('The division of {0} and {1} is {2}'.format(num1, num2, div))
---
+++ even.py
@@ -0,0 +1,8 @@
+#Python Program to check whether a number is even or not.  
+num = int(input("enter the number:"))         
+
+if num%2 == 0:      
+
+    print("The entered number is an even number")    
+else:    
+    print("The entered Number is an odd number") 
